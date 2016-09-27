655f3777a62435fedb2e09d65e6eb4d7fbd8adf6
Your Name <you@example.com>
2016-09-26 21:56:19 -0400

    name fix

--- Analysis_dissertaiton_ENG.R
+++ Analysis_dissertaiton_ENG.R
@@ -2,16 +2,16 @@ library(lme4)
 whole = read.csv("/Users/chongzhang/Desktop/ENG.csv")
 summary(whole)
 head(whole)
-SS = subset(whole, Rctype == "SS")
+SS = subset(whole, RCtype == "SS")
 SS
-SO = subset(whole, Rctype == "SO")
-OS = subset(whole, Rctype == "OS")
-OO = subset(whole, Rctype == "OO")
+SO = subset(whole, RCtype == "SO")
+OS = subset(whole, RCtype == "OS")
+OO = subset(whole, RCtype == "OO")
 
 #ARC_EQUI_INANIM_NP2=subset(xMain, Rctype == "ACTIVE" & Plausibility == "EQUI" & Animacy == "inanimate")$NP2RT
 #l20 = lmer(PRE_TAR ~ Plausibility*Rctype*Animacy + (1|Subjects)+(1|Items), xMainDem)
 
-lR3 = lmer (log_R3 ~ Rctype*Matching + (1|Participant)+(1|Item), whole)
+lR3 = lmer (log_R3 ~ RCtype*Matching + (1|Participant)+(1|Item), whole)
 lR3
 
 S_V_in_RC1 = subset(whole, RC1 == "S")$log_R3
