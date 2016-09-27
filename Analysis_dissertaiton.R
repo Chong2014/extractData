3ac2f5ed861428ad8f74c7c461f937fbae22c7e7
ChongZhang <zhangchong10@gmail.com>
2016-09-26 21:14:13 -0400

    ln to log

--- Analysis_dissertaiton.R
+++ Analysis_dissertaiton.R
@@ -11,9 +11,9 @@ OO = subset(whole, Rctype == "OO")
 #ARC_EQUI_INANIM_NP2=subset(xMain, Rctype == "ACTIVE" & Plausibility == "EQUI" & Animacy == "inanimate")$NP2RT
 #l20 = lmer(PRE_TAR ~ Plausibility*Rctype*Animacy + (1|Subjects)+(1|Items), xMainDem)
 
-lR3 = lmer (ln_R3 ~ Rctype*Matching + (1|Participant)+(1|Item), whole)
+lR3 = lmer (log_R3 ~ Rctype*Matching + (1|Participant)+(1|Item), whole)
 lR3
 
-S_V_in_RC1 = subset(whole, RC1 == "S")$ln_R3
-O_V_in_RC1 = subset(whole, RC1 == "O")$ln_R4
+S_V_in_RC1 = subset(whole, RC1 == "S")$log_R3
+O_V_in_RC1 = subset(whole, RC1 == "O")$log_R4
 t.test(S_V_in_RC1, O_V_in_RC1)
