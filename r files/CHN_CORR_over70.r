13d75a11a4ed4d2925df90cf7861fe0189d85104
Chong Zhang <zhangchong10@gmail.com>
2016-10-02 19:47:51 -0400

    update

--- r files/CHN_CORR_over70.r
+++ r files/CHN_CORR_over70.r
@@ -48,62 +48,71 @@ t.test(RCs_SS, RCs_OS) #not sig.
 t.test(RCs_SO, RCs_OO) #sig. t = 2.3459, df = 777.91, p-value = 0.01923
 
 #Verb in RC1:
-RC1_verb_S = subset(wholeENG_CORR_over70, RC1 == "S")$log_R3
-RC1_verb_O = subset(wholeENG_CORR_over70, RC1 == "O")$log_R4
-t.test(RC1_verb_S, RC1_verb_O) #t = -17.941, df = 1257.4, p-value < 2.2e-16
-RC1_verb_SS = subset(wholeENG_CORR_over70, RC1 == "S" & RC2 == "S")$log_R3
-RC1_verb_SO = subset(wholeENG_CORR_over70, RC1 == "S" & RC2 == "O")$log_R3
-RC1_verb_OS = subset(wholeENG_CORR_over70, RC1 == "O" & RC2 == "S")$log_R4
-RC1_verb_OO = subset(wholeENG_CORR_over70, RC1 == "O" & RC2 == "O")$log_R4
+RC1_verb_S = subset(wholeCHN_CORR_over70, RC1 == "S")$log_R3
+RC1_verb_O = subset(wholeCHN_CORR_over70, RC1 == "O")$log_R4
+t.test(RC1_verb_S, RC1_verb_O) #not sig.
+RC1_verb_SS = subset(wholeCHN_CORR_over70, RC1 == "S" & RC2 == "S")$log_R3
+RC1_verb_SO = subset(wholeCHN_CORR_over70, RC1 == "S" & RC2 == "O")$log_R3
+RC1_verb_OS = subset(wholeCHN_CORR_over70, RC1 == "O" & RC2 == "S")$log_R4
+RC1_verb_OO = subset(wholeCHN_CORR_over70, RC1 == "O" & RC2 == "O")$log_R4
 t.test(RC1_verb_SS, RC1_verb_SO) #not sig.
 t.test(RC1_verb_OS, RC1_verb_OO) #not sig.
-t.test(RC1_verb_SO, RC1_verb_OO) #t = -11.609, df = 643.49, p-value < 2.2e-16
+t.test(RC1_verb_SS, RC1_verb_OO) #not sig.
 
 #Verb in RC2:
-RC2_verb_S = subset(wholeENG_CORR_over70, RC2 == "S")$log_R7
-RC2_verb_O = subset(wholeENG_CORR_over70, RC2 == "O")$log_R8
-t.test(RC2_verb_S, RC2_verb_O) #t = 3.6663, df = 1406.1, p-value = 0.0002552
-RC2_verb_SS = subset(wholeENG_CORR_over70, RC1 == "S" & RC2 == "S")$log_R7
-RC2_verb_SO = subset(wholeENG_CORR_over70, RC1 == "S" & RC2 == "O")$log_R8
-RC2_verb_OS = subset(wholeENG_CORR_over70, RC1 == "O" & RC2 == "S")$log_R7
-RC2_verb_OO = subset(wholeENG_CORR_over70, RC1 == "O" & RC2 == "O")$log_R8
-t.test(RC2_verb_SS, RC2_verb_SO) #t = 3.0541, df = 706.83, p-value = 0.002342
-t.test(RC2_verb_OS, RC2_verb_OO) #t = 2.1477, df = 697.42, p-value = 0.03208
-t.test(RC2_verb_SO, RC2_verb_OO) #not sig
-t.test(RC2_verb_SO, RC2_verb_OS) #not sig
-t.test(RC2_verb_SS, RC2_verb_OS) #marginal t = 1.8623, df = 716.66, p-value = 0.06297
+RC2_verb_S = subset(wholeCHN_CORR_over70, RC2 == "S")$log_R8
+RC2_verb_O = subset(wholeCHN_CORR_over70, RC2 == "O")$log_R9
+t.test(RC2_verb_S, RC2_verb_O) #t = -4.4937, df = 1557.4, p-value = 7.516e-06
+RC2_verb_SS = subset(wholeCHN_CORR_over70, RC1 == "S" & RC2 == "S")$log_R8
+RC2_verb_SO = subset(wholeCHN_CORR_over70, RC1 == "S" & RC2 == "O")$log_R9
+RC2_verb_OS = subset(wholeCHN_CORR_over70, RC1 == "O" & RC2 == "S")$log_R8
+RC2_verb_OO = subset(wholeCHN_CORR_over70, RC1 == "O" & RC2 == "O")$log_R9
+t.test(RC2_verb_SS, RC2_verb_SO) #sig. t = -5.1314, df = 760.63, p-value = 3.654e-07; 2.783514  2.882948 
+t.test(RC2_verb_OS, RC2_verb_OO) #not sig. t = -1.3522, df = 771.56, p-value = 0.1767;  2.833016  2.860857 
+#SS *>> OS >> OO >> SO
+ t.test(RC2_verb_SO, RC2_verb_OO) #not sig.
+t.test(RC2_verb_SS, RC2_verb_OS) #sig. t = -2.5043, df = 752.48, p-value = 0.01248
+t.test(RC2_verb_SO, RC2_verb_OS) #sig. t = 2.3605, df = 777.1, p-value = 0.0185
 
 #edge of RC1
-RC1_edge_S = subset(wholeENG_CORR_over70, RC1 == "S")$log_R3
-RC1_edge_O = subset(wholeENG_CORR_over70, RC1 == "O")$log_R3
-t.test(RC1_edge_S, RC1_edge_O) #t = -3.9891, df = 1409.7, p-value = 6.973e-05
-RC1_edge_SS = subset(wholeENG_CORR_over70, RC1 == "S" & RC2 == "S")$log_R3
-RC1_edge_SO = subset(wholeENG_CORR_over70, RC1 == "S" & RC2 == "O")$log_R3
-RC1_edge_OS = subset(wholeENG_CORR_over70, RC1 == "O" & RC2 == "S")$log_R3
-RC1_edge_OO = subset(wholeENG_CORR_over70, RC1 == "O" & RC2 == "O")$log_R3
+RC1_edge_S = subset(wholeCHN_CORR_over70, RC1 == "S")$log_R3
+RC1_edge_O = subset(wholeCHN_CORR_over70, RC1 == "O")$log_R3
+t.test(RC1_edge_S, RC1_edge_O) #t = -4.4019, df = 1556.2, p-value = 1.146e-05
+RC1_edge_SS = subset(wholeCHN_CORR_over70, RC1 == "S" & RC2 == "S")$log_R3
+RC1_edge_SO = subset(wholeCHN_CORR_over70, RC1 == "S" & RC2 == "O")$log_R3
+RC1_edge_OS = subset(wholeCHN_CORR_over70, RC1 == "O" & RC2 == "S")$log_R3
+RC1_edge_OO = subset(wholeCHN_CORR_over70, RC1 == "O" & RC2 == "O")$log_R3
 t.test(RC1_edge_SS, RC1_edge_SO) #not sig.
 t.test(RC1_edge_OS, RC1_edge_OO) #not sig.
-t.test(RC1_edge_SO, RC1_edge_OO) #not sig.
-#edge of RC2
-RC2_edge_S = subset(wholeENG_CORR_over70, RC2 == "S")$log_R7
-RC2_edge_O = subset(wholeENG_CORR_over70, RC2 == "O")$log_R7
-t.test(RC2_edge_S, RC2_edge_O) #not significant, t = -0.31733, df = 1426.1, p-value = 0.751
-RC2_edge_SS = subset(wholeENG_CORR_over70, RC1 == "S" & RC2 == "S")$log_R7
-RC2_edge_SO = subset(wholeENG_CORR_over70, RC1 == "S" & RC2 == "O")$log_R7
-RC2_edge_OS = subset(wholeENG_CORR_over70, RC1 == "O" & RC2 == "S")$log_R7
-RC2_edge_OO = subset(wholeENG_CORR_over70, RC1 == "O" & RC2 == "O")$log_R7
-t.test(RC2_edge_SS, RC2_edge_SO) #not sig.
-t.test(RC2_edge_OS, RC2_edge_OO) #not sig.
-t.test(RC2_edge_SS, RC2_edge_OS) #not sig.
+#SS >> SO *>> OO >> OS
+t.test(RC1_edge_SS, RC1_edge_OS) #sig. t = -3.5157, df = 777.87, p-value = 0.000464
+t.test(RC1_edge_SS, RC1_edge_OO) #sig. t = -3.5318, df = 777.99, p-value = 0.000437
+t.test(RC1_edge_SO, RC1_edge_OO) #sig. t = -2.6927, df = 775.35, p-value = 0.00724
 
-#main verb
-main_verb_SS = subset(wholeENG_CORR_over70, RC1 == "S" & RC2 == "S")$log_R10
-main_verb_SO = subset(wholeENG_CORR_over70, RC1 == "S" & RC2 == "O")$log_R10
-main_verb_OS = subset(wholeENG_CORR_over70, RC1 == "O" & RC2 == "S")$log_R10
-main_verb_OO = subset(wholeENG_CORR_over70, RC1 == "O" & RC2 == "O")$log_R10
-t.test(main_verb_SS, main_verb_SO) #not sig.
-t.test(main_verb_OS, main_verb_OO) #not sig.
-#OS >> SS >> OO >> SO
-t.test(main_verb_OS, main_verb_SO) #t = -2.1665, df = 674.05, p-value = 0.03062
+#edge of RC2
+RC2_edge_S = subset(wholeCHN_CORR_over70, RC2 == "S")$log_R8
+RC2_edge_O = subset(wholeCHN_CORR_over70, RC2 == "O")$log_R8
+t.test(RC2_edge_S, RC2_edge_O) #sig. t = -5.6476, df = 1545.1, p-value = 1.933e-08
+RC2_edge_SS = subset(wholeCHN_CORR_over70, RC1 == "S" & RC2 == "S")$log_R8
+RC2_edge_SO = subset(wholeCHN_CORR_over70, RC1 == "S" & RC2 == "O")$log_R8
+RC2_edge_OS = subset(wholeCHN_CORR_over70, RC1 == "O" & RC2 == "S")$log_R8
+RC2_edge_OO = subset(wholeCHN_CORR_over70, RC1 == "O" & RC2 == "O")$log_R8
+t.test(RC2_edge_SS, RC2_edge_SO) #sig. t = -5.1123, df = 750.07, p-value = 4.044e-07
+t.test(RC2_edge_OS, RC2_edge_OO) #sig. t = -2.9832, df = 777.87, p-value = 0.002941
+#SS *>> OS *>> SO > OO
+t.test(RC2_edge_SS, RC2_edge_OS) #sig. t = -2.5043, df = 752.48, p-value = 0.01248
+t.test(RC2_edge_SO, RC2_edge_OS) #sig. t = 2.411, df = 777.94, p-value = 0.01614
+t.test(RC2_edge_SO, RC2_edge_OO) #not sig.
+
+#head_NP
+head_NP_SS = subset(wholeCHN_CORR_over70, RC1 == "S" & RC2 == "S")$log_R11
+head_NP_SO = subset(wholeCHN_CORR_over70, RC1 == "S" & RC2 == "O")$log_R11
+head_NP_OS = subset(wholeCHN_CORR_over70, RC1 == "O" & RC2 == "S")$log_R11
+head_NP_OO = subset(wholeCHN_CORR_over70, RC1 == "O" & RC2 == "O")$log_R11
+t.test(head_NP_SS, head_NP_SO) #sig. t = -5.4092, df = 764.09, p-value = 8.475e-08
+t.test(head_NP_OS, head_NP_OO) #sig. t = 2.522, df = 774.14, p-value = 0.01187
+#SS >> OO *>> OS >> SO
+t.test(head_NP_SS, head_NP_OO) #not sig.
+t.test(head_NP_OS, head_NP_SO) #not sig.
 
 
